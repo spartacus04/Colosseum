06f2f22963df1929767b717e4e3bd9c8ea76d0d0
spartacus04 <andrea.bonari.pingas@gmail.com>
2025-02-14 01:00:57 +0100

    Add icon

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 dokka {
     pluginsConfiguration.html {
         customStyleSheets.from(file("docsAssets/logo-styles.css"))
-        customAssets.from(file("icon.png"))
+        customAssets.from(file("icon.webp"))
         footerMessage = "Colosseum is licensed under the <a href=\"https://github.com/spartacus04/Colosseum/blob/master/LICENSE.MD\">MIT</a> License."
 
     }
